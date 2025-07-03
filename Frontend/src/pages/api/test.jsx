3840ff0e1f87896d03dfbd652ef4b61a3c72c4ae
yosef-agil <yosefagil8@agil.com>
2025-07-03 21:09:28 +0700

    update

--- Frontend/src/pages/api/test.jsx
+++ Frontend/src/pages/api/test.jsx
@@ -1,7 +1,1 @@
-import axios from 'axios';
-
-const API_URL = import.meta.env.VITE_API_URL;
-
-export const getTest = () => {
-  return axios.get(`${API_URL}/test`);
-};
+const API_URL = import.meta.env.VITE_API_URL || "https://backendinv-production.up.railway.app";
