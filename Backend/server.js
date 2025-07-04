677c14531fb8761e20730a0c8a86310b08516ee5
yosef-agil <yosefagil8@agil.com>
2025-07-04 23:54:27 +0700

    add: cors

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mysql = require('mysql2');
 require('dotenv').config();
+const cors = require('cors');
 const util = require("util");
 
 const app = express();
